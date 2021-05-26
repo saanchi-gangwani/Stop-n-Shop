1326a19b1769631baebc371f6f9312f5b56a547c
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-05-27 01:48:10 +0530

    minor

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ session_start();
   <head>
     <meta charset="utf-8">
     <title>Stop n Shop: Login</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/enter.css">
   </head>
   <body>
     <div class="bodydiv">
