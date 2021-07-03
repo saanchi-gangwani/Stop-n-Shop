d26c78e2b906965a2a2ff8d5702a034c7a286ebb
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-07-04 01:41:02 +0530

    Header 2/3

--- header.php
+++ header.php
@@ -7,13 +7,74 @@
     <div class="navdiv">
       <nav>
         <div class="navelementdiv">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" class="svg-inline--fa fa-home fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>HOME
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" class="svg-inline--fa fa-home fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>Home
         </div>
         <div class="navelementdiv">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" class="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>PROFILE
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" class="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>Profile
         </div>
         <div class="navelementdiv">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-cart" class="svg-inline--fa fa-shopping-cart fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"></path></svg>CART
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-cart" class="svg-inline--fa fa-shopping-cart fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"></path></svg>Cart
+        </div>
+        <div class="navelementdiv">
+          <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+          <svg
+             xmlns:dc="http://purl.org/dc/elements/1.1/"
+             xmlns:cc="http://creativecommons.org/ns#"
+             xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+             xmlns:svg="http://www.w3.org/2000/svg"
+             xmlns="http://www.w3.org/2000/svg"
+             xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+             xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+             inkscape:version="1.0 (4035a4fb49, 2020-05-01)"
+             height="384.06378"
+             width="503.97501"
+             sodipodi:docname="temp.svg"
+             id="svg845"
+             version="1.1"
+             viewBox="0 0 503.97501 384.06378"
+             role="img"
+             class="svg-inline--fa fa-sign-out-alt fa-w-16"
+             data-icon="sign-out-alt"
+             data-prefix="fas"
+             focusable="false"
+             aria-hidden="true">
+            <metadata
+               id="metadata851">
+              <rdf:RDF>
+                <cc:Work
+                   rdf:about="">
+                  <dc:format>image/svg+xml</dc:format>
+                  <dc:type
+                     rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                </cc:Work>
+              </rdf:RDF>
+            </metadata>
+            <defs
+               id="defs849" />
+            <sodipodi:namedview
+               inkscape:current-layer="svg845"
+               inkscape:window-maximized="1"
+               inkscape:window-y="-9"
+               inkscape:window-x="-9"
+               inkscape:cy="192.02127"
+               inkscape:cx="256"
+               inkscape:zoom="1.6210938"
+               showgrid="false"
+               id="namedview847"
+               inkscape:window-height="1001"
+               inkscape:window-width="1920"
+               inkscape:pageshadow="2"
+               inkscape:pageopacity="0"
+               guidetolerance="10"
+               gridtolerance="10"
+               objecttolerance="10"
+               borderopacity="1"
+               bordercolor="#666666"
+               pagecolor="#ffffff" />
+            <path
+               id="path843"
+               d="m 497,209.02127 -168,168 c -15,15 -41,4.5 -41,-17 v -96 H 152 c -13.3,0 -24,-10.7 -24,-24 v -96 c 0,-13.3 10.7,-24 24,-24 H 288 V 24.021269 C 288,2.6212687 313.9,-7.9787313 329,7.0212687 L 497,175.02127 c 9.3,9.4 9.3,24.6 0,34 z m -305,163 v -40 c 0,-6.6 -5.4,-12 -12,-12 H 96 c -17.7,0 -32,-14.3 -32,-32 V 96.021269 c 0,-17.7 14.3,-32 32,-32 h 84 c 6.6,0 12,-5.4 12,-12 v -40 C 192,5.4212687 186.6,0.02126866 180,0.02126866 H 96 C 43,0.02126866 0,43.021269 0,96.021269 V 288.02127 c 0,53 43,96 96,96 h 84 c 6.6,0 12,-5.4 12,-12 z" />
+          </svg>Logout
         </div>
       </nav>
     </div>
