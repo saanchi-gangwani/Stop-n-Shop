7902909e88163c3415c3cd72c02ca642fa986e96
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-07-16 21:13:18 +0530

    Cart Price Update done

--- js/master.js
+++ js/master.js
@@ -21,13 +21,25 @@ function update2Cart(value){
   id=value.substring(0,value.length-1);
   operation = value.substring(value.length-1);
   quant = document.getElementById('cartvalue_'+id).innerHTML.trim();
-  if(quant==='1' && operation==='-'){
-    document.getElementById('cartproductdiv_'+id).remove();
+  if(operation==='-'){
+    if(quant==='1'){
+      document.getElementById('cartproductdiv_'+id).remove();
+    }
+    else{
+      quantPrice = parseFloat(document.getElementById('pricevalue_'+id).innerHTML.trim().substring(2));
+      price = quantPrice/parseFloat(quant);
+      document.getElementById('pricevalue_'+id).innerHTML = "  &#8377; "+(quantPrice-price)+".00";
+    }
+  }
+  else{
+    quantPrice = parseFloat(document.getElementById('pricevalue_'+id).innerHTML.trim().substring(2));
+    price = quantPrice/parseFloat(quant);
+    document.getElementById('pricevalue_'+id).innerHTML = "  &#8377; "+(quantPrice+price)+".00";
   }
 
   updateCart(value);
 
-  // enter price ajax function here
+
 
   cartdisplaydiv = document.getElementById('cartdisplaydiv');
   if(cartdisplaydiv.innerHTML.trim()===""){
@@ -39,6 +51,6 @@ function createNoCartDiv(){
 return '<div class="nocartdiv">'+
         '<span>Your cart is empty :( </span>'+
         '<span>You may go to Stop n Shop home page to explore more products ;)</span>'+
-        '<button type="button" name="button" onclick="window.location.replace(\'home.php\')">Home page</button>'+
+        '<button type="button" name="button" onclick="window.location.replace(\'home.php\')">Go to Home page</button>'+
        '</div>';
 }
