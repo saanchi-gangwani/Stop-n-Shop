98510e9959ff17cfc2b03b8133ababa1e2ba9d64
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2021-07-24 18:09:54 +0530

    Minor

--- aboutus.php
+++ aboutus.php
@@ -22,9 +22,22 @@ if(!isset($_SESSION['useremail']))
       <div class="contentdiv">
         <div class="aboutdiv">
           <h3>About Us</h3>
+          <p>
+            Welcome to Stop n Shop! SNS is a sample E-commerce website whose backend is completely built on Laravel PHP and MySQL Database. On the frontside, the website uses HTML, CSS, Vanilla JavaScript and some frontend frameworks like Bootstrap and jQuery.
+          </p>
+          <p>
+            The site has all the major features of a real-world E-commerce website including registration and logging in of users, cart management, profile management, storage of multiple addresses etc.
+          </p>
         </div>
         <div class="contactdiv">
           <h3>Contact Us</h3>
+          <p>
+            Visit the developers of this website on their GitHub:
+            <ul>
+              <li><a href='https://github.com/devoghub'>Devjyot Singh Sidhu</a></li>
+              <li><a href='https://github.com/saanchi-gangwani/'>Saanchi Gangwani</a></li>
+            </ul>
+          </p>
         </div>
       </div>
       <?php include(__DIR__.'/php/footer.php'); ?>
